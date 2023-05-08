1f4bc30448f700b9db80ad7786168abd10715e77
zikirazizi <zikir.azizi.ft22@mail.umy.ic.id>
2023-05-08 16:26:48 +0700

    data mahasiswa

--- pointer.cpp_205/pointer.cpp_205.cpp
+++ pointer.cpp_205/pointer.cpp_205.cpp
@@ -16,12 +16,5 @@ int main()
 	mahasiswa mhs{ 1 };     //object mhs
 	mhs.showNim();          // Member Acces Operator
 
-	mahasiswa& ref = mhs;   // Pointer Reference refMhs
-	ref.nim = 2;            //Member Acces Operator
-
-	mahasiswa* pMhs = &mhs; // Pointer Deferences pMhs
-	pMhs->nim = 3;          //Arrow Operator
-	mhs.showNim();
-	return 0;
-
+	
 }
